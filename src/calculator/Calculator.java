2eb343b4cbf3c1cffd6499af2a93acda91492f70
Donghee Lee <dhlee8461@gmail.com>
2026-03-25 20:27:02 +0900

    Calculator.java -> App.java 신규 Calculator.java 생성

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,37 +1,29 @@
 package calculator;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = "";  //변수 선언 후 초기화
+    private List<Double> results = new ArrayList<>(); // 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
 
-        while (!input.equals("exit")) {
-            System.out.print("숫자 입력: ");
-            int num1 = scanner.nextInt();
-            System.out.print("기호 입력: ");
-            char cal = scanner.next().charAt(0);  //공백이전까지의 문자열 입력받고 변수에 저장
-            System.out.print("숫자 입력: ");
-            int num2 = scanner.nextInt();
+    public double calculate(int num1, int num2, char cal) {
 
-            int result = 0;
-            if (cal == '+') {
-                result = num1 + num2;
-            } else if (cal == '-') {
-                result = num1 - num2;
-            } else if (cal == '*') {
-                result = num1 * num2;
-            } else if (cal == '/') {
-                if (num2 != 0) {          //입력값이 0이 아닐 때
-                    result = num1 / num2;
-                } else {                // 나눗셈시 분모가 0인 경우 방지
-                    System.out.println("0으로 나눌 수 없습니다.");
-                }
+        double result = 0;
+
+        if (cal == '+') {
+            result = num1 + num2;
+        } else if (cal == '-') {
+            result = num1 - num2;
+        } else if (cal == '*') {
+            result = num1 * num2;
+        } else if (cal == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {            // 나눗셈시 분모가 0인 경우 방지
+                System.out.println("0으로 나눌 수 없습니다.");
+                return 0; // 나눗셈 오류로 반환 값이 없을 때 0반환
             }
-            System.out.println("결과: " + result);
-
-            System.out.println("더 계산하시겠습니까? 아무키나 입력하세요. (exit 입력 시 종료)");
-            input = scanner.next();    // 이미 위에 input이 선언됐기 때문에 자료형 필요 x
         }
+        results.add(result);
+        return result;
     }
 }
