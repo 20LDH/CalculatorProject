0ae1cf4c8aeb256c652cec18925fe065786183d3
Donghee Lee <dhlee8461@gmail.com>
2026-04-02 12:54:31 +0900

    입력받은 값보다 큰 결과값 출력

--- src/calculator2/step3/App.java
+++ src/calculator2/step3/App.java
@@ -1,5 +1,6 @@
 package calculator2.step3;
 import java.util.Scanner;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
@@ -9,7 +10,7 @@ public class App {
 
         while (!input.equals("exit")) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            double num1 = sc.nextInt();
+            double num1 = sc.nextDouble();
             // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
             if (num1 < 0) {
@@ -21,7 +22,7 @@ public class App {
             OperatorType operator = OperatorType.fromSymbol(symbol);
 
             System.out.print("두 번째 숫자를 입력하세요: ");
-            double num2 = sc.nextInt();
+            double num2 = sc.nextDouble();
             // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
             if (num2 < 0) {
@@ -33,12 +34,21 @@ public class App {
 
             System.out.println("결과 = " + calculate);
             System.out.println("현재까지 저장된 결과들: " + calculator.getResults());
-            System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? 네 / 아니요 ");
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? 네 / 아니요");
             String removeInput = sc.next();
             if ("네".equals(removeInput)) {
                 calculator.removeResult();  // 컬렉션에서 가장 먼저 들어온 값 제거
                 System.out.println("가장 먼저 저장된 결과가 삭제되었습니다.");
             }
+            System.out.println("정수를 입력하고 그보다 큰 결과값만 출력하시겠습니까 네 / 아니요");
+            String answer = sc.next();
+            if ("네".equals(answer)) {
+                System.out.println("정수를 입력하세요: ");
+                double threshold = sc.nextDouble();
+                List<Double> filteredList = calculator.getResultsBig(threshold); // threshold : 결과값, 임계값
+                System.out.println(threshold + "보다 큰 결과값: " + filteredList);
+            }
+
             System.out.println("종료하려면 exit 입력 그렇지 않으면 아무 글자나 입력하세요.");
             input = sc.next();
         }
--- src/calculator2/step3/ArithmeticCalculator.java
+++ src/calculator2/step3/ArithmeticCalculator.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public class ArithmeticCalculator {
     private List<Double> results = new ArrayList<>();
-    public  Double calculate(double num1, double num2, OperatorType type) {
+
+    public Double calculate(double num1, double num2, OperatorType type) {
         double result = 0;
 
 
@@ -21,8 +22,7 @@ public class ArithmeticCalculator {
             } else {            // 나눗셈시 분모가 0인 경우 방지
                 throw new ArithmeticException(("0으로 나눌 수 없습니다."));
             }
-        }
-        else {
+        } else {
             throw new ArithmeticException(("사친연산 기호가 아닙니다."));
         }
         results.add(result);  // 결과 저장하기 위함
@@ -30,11 +30,19 @@ public class ArithmeticCalculator {
     }
 
     public List<Double> getResults() {  //
+
         return results;
     }
+
     public void removeResult() {     //기록 삭제 기능
         if (!results.isEmpty()) {    // 컬렉션이 비어있지 않은지 확인
             results.remove(0);  // 컬렉션에서 가장 먼저 들어온 값 제거
         }
+    }
+
+    public List<Double> getResultsBig(double threshold) {
+        return results.stream()
+                .filter(result -> result > threshold) // filter() : 조건에 맞는 데이터만 남긴다
+                .toList(); //리스트로 반환
     }
 }
